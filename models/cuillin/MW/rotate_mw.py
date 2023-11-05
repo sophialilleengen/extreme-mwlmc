36e45a007b89664da3b12fff11df3e4f986d46d1
michael_petersen <petersen.michael.s@gmail.com>
2023-11-05 10:39:05 +0000

    early outcoef tests for mwlmc

--- models/cuillin/MW/rotate_mw.py
+++ models/cuillin/MW/rotate_mw.py
@@ -13,19 +13,19 @@ from exptool.observables import transform
 
 comp = 'mwdisc'
 
-I = particle.Input('OUT.runmw0.00000',comp,legacy=True)
+I = particle.Input('MW0/OUT.runmw0.00000',comp,legacy=True)
 nbodies = I.header[comp]['nbodies']
 
 # this rotation brings the disc to the x-z plane
-O = transform.rotate_points(PSPDump,90.,0.,0.)
+O = transform.rotate_points(I,90.,0.,0.)
 
 # also do the rotation here: all particles at once.
 newx,newy,newz = O.xpos,O.ypos,O.zpos
 newvx,newvy,newvz = O.xvel,O.yvel,O.zvel
 # add the translation
 
-f = open('transformed{}.bods'.format(comp),'w')
+f = open('MW0/transformed{}.bods'.format(comp),'w')
 print('{} 0 0'.format(nbodies),file=f)
 for i in range(0,nbodies):
-    print(I.data['m'][i],newx[i],newy[i],newz[i],newvx[i],newvy[i],newvz[i],file=f)
+    print(I.mass[i],newx[i],newy[i],newz[i],newvx[i],newvy[i],newvz[i],file=f)
 f.close()
